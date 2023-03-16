23bbe3b30aa1d0701cfacc56123fb9f791e5faf1
Daniil Rassadin <daniil@rassadin.org>
2023-03-16 15:37:48 +0600

    TableView added

---
+++ Notes/Model.swift
@@ -0,0 +1,24 @@
+//
+//  Model.swift
+//  Notes
+//
+//  Created by Daniil Rassadin on 16/3/23.
+//
+
+import Foundation
+
+var notes: [String] = ["First note", "Another one very biiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiig note", "And one more", "Very big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big big  note"]
+
+func addNote(to array: inout [String], note: String) {
+    array.insert(note, at: 0)
+    saveData()
+}
+
+func deleteNote(in array: inout [String], at index: Int) {
+    array.remove(at: index)
+    saveData()
+}
+
+func saveData() {
+    
+}
---
+++ Notes/NotesVC.swift
@@ -0,0 +1,98 @@
+//
+//  NotesVC.swift
+//  Notes
+//
+//  Created by Daniil Rassadin on 16/3/23.
+//
+
+import UIKit
+
+class NotesVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.rowHeight = 65
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return notes.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = notes[indexPath.row]
+        
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let alertController = UIAlertController(title: nil, message: "Are you sure you want to delete the note?", preferredStyle: .alert)
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+            let deleteAction = UIAlertAction(title: "Delete Note", style: .destructive) {_ in
+                deleteNote(in: &notes, at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            
+            alertController.addAction(deleteAction)
+            alertController.addAction(cancelAction)
+            present(alertController, animated: true)
+            
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
